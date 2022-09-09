4932e29f2a4ff8cd9b9b206e6e897156ef1cace6
Emre <matizdrum@gmail.com>
2022-09-09 22:24:09 +0300

    computerChoice function added

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let playerScore = 0
 let computerScore = 0
 
 const buttons = document.querySelectorAll('input')
+
+function computerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
